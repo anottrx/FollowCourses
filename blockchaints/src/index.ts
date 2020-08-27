4d8e0a0bcfb625438b5ee3e8ce243c09a17326d9
anottrx <anottrx@gmail.com>
2020-08-27 22:15:01 +0900

    #0 TypeScript: Make Class

--- blockchaints/src/index.ts
+++ blockchaints/src/index.ts
@@ -1,25 +1,33 @@
-interface Human {
-  // 오직 TS에서만 가능하다. JS에서는 작동하지 않는다.
-  name: string;
-  age: number;
-  gender: string;
+class Human {
+  public name: string;
+  public age: number;
+  public gender: string;
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
 }
 
-const person = {
-  name: "heyyyy",
-  gender: "male",
-  age: 22,
-};
+/*
+interface Human {
+  // 오직 TS에서만 가능하다. JS에서는 작동하지 않는다.
+  name: string; age: number; gender: string;
+} 
+*/
+
+const heyyyy = new Human("heyyyy", 20, "female");
+// const person = { name: "heyyyy", gender: "male", age: 22 };
 
 const sayHi = (person: Human): string => {
   // const sayHi = (name: string, age: number, gender: string): string => {
-  // 끝에 ?가 붙으면 선택임을 알려준다. (예시: gender?)
-  // JS와는 다르게 TS는 파라미터 값에 문제가 생길 경우 에러가 난다.
+  // 끝에 ?가 붙으면 선택임을 알려준다. (예시: gender?) JS와는 다르게 TS는 파라미터 값에 문제가 생길 경우 에러가 난다.
   // return `Hello ${name}, you are ${age}, you are a ${gender}`;
   return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
 };
 
-console.log(sayHi(person));
+console.log(sayHi(heyyyy));
+// console.log(sayHi(person));
 // console.log(sayHi("heyyyy", 24, "male"));
 
 export {};
