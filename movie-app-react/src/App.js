7567c31a4a94e3e1f0c4f44cb55780cee7af50a7
anottrx <anottrx@gmail.com>
2020-08-29 22:22:49 +0900

    #1 Movie Component, API
    fetch와 유사한 axios 사용

--- movie-app-react/src/App.js
+++ movie-app-react/src/App.js
@@ -1,41 +1,21 @@
 import React from "react";
-import PropTypes from "prop-types";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    count: 0,
+    isLoading: true,
+    movies: [],
   };
-
-  add = () => {
-    this.setState((current) => ({ count: this.state.count + 1 }));
-    // setState를 호출할 때마다 react는 새 state 호출과 render()를 다시 실행.
+  getMovies = async () => {
+    // axios의 속도가 느릴 수 있기 때문에 기다려야 한다
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json");
   };
-  minus = () => {
-    this.setState((current) => ({ count: this.state.count - 1 }));
-  };
-
   componentDidMount() {
-    console.log("component rendered");
+    this.getMovies();
   }
-
-  componentDidUpdate() {
-    console.log("I've just updated");
-  }
-
-  componentWillUnmount() {
-    // 다른 페이지로 이동 등의 행동할 때
-    console.log("Goodbye");
-  }
-
   render() {
-    console.log("I'm rendering");
-    return (
-      <div>
-        <h1>The number is: {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-      </div>
-    );
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : "We are ready"}</div>;
   }
 }
 
