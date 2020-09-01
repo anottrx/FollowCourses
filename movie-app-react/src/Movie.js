c626d57489c48182579d63ee5da0f4c77aa37ca9
anottrx <anottrx@gmail.com>
2020-09-01 23:59:04 +0900

    #2 CSS

--- movie-app-react/src/Movie.js
+++ movie-app-react/src/Movie.js
@@ -9,7 +9,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="genres">
+        <ul className="movie__genres">
           {genres.map((genre, index) => (
             //   map()은 index를 넘겨준다
             <li key={index} className="genres__genre">
@@ -17,7 +17,7 @@ function Movie({ id, year, title, summary, poster, genres }) {
             </li>
           ))}
         </ul>
-        <p className="movie__summary">{summary.slice(0, 180)}</p>
+        <p className="movie__summary">{summary.slice(0, 180)}...</p>
       </div>
     </div>
   );
