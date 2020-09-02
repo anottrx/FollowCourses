b71cc0c3571a5d8d6878d1bba20c3db301ee6e67
anottrx <anottrx@gmail.com>
2020-09-02 22:28:25 +0900

    Updates

---
+++ precache-manifest.bda6c45baf7c4cf82437d0e34a9a4492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ed2056e6c811582fcf7d916e117ae53",
+    "url": "/movie-app-react/index.html"
+  },
+  {
+    "revision": "67c99bd2c124e631190e",
+    "url": "/movie-app-react/static/css/main.9b26caff.chunk.css"
+  },
+  {
+    "revision": "03d89e0e5482b5060425",
+    "url": "/movie-app-react/static/js/2.80117928.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie-app-react/static/js/2.80117928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c99bd2c124e631190e",
+    "url": "/movie-app-react/static/js/main.23d8f22f.chunk.js"
+  },
+  {
+    "revision": "64e0037c206578654b3b",
+    "url": "/movie-app-react/static/js/runtime-main.c5c16297.js"
+  }
+]);
