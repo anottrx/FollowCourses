c74ad8bf81ffbe0d18a4f90383de71e0819a714c
anottrx <anottrx@gmail.com>
2020-09-04 22:21:54 +0900

    #3 Solve Error
    경로 및 CSS 수정
    CSS 인식 에러 해결

--- movie-app-react/src/components/Movie.js
+++ movie-app-react/src/components/Movie.js
@@ -2,14 +2,14 @@ import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
 
-function Movie({ id, year, title, summary, poster, genres }) {
+function Movie({ year, title, summary, poster, genres }) {
   return (
     <div className="movie">
       <img src={poster} alt={title} title={title} />
       <div className="movie__data">
         <h3 className="movie__title">{title}</h3>
         <h5 className="movie__year">{year}</h5>
-        <ul className="movie__genres">
+        <ul className="genres">
           {genres.map((genre, index) => (
             //   map()은 index를 넘겨준다
             <li key={index} className="genres__genre">
