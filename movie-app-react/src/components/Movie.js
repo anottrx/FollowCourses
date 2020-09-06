9c8bd50099cced2639608c31d3b633c0c854d399
anottrx <anottrx@gmail.com>
2020-09-06 22:02:52 +0900

    #4 Solve Error

--- movie-app-react/src/components/Movie.js
+++ movie-app-react/src/components/Movie.js
@@ -18,21 +18,19 @@ function Movie({ id, year, title, summary, poster, genres }) {
           },
         }}
       >
-        <div className="movie">
-          <img src={poster} alt={title} title={title} />
-          <div className="movie__data">
-            <h3 className="movie__title">{title}</h3>
-            <h5 className="movie__year">{year}</h5>
-            <ul className="genres">
-              {genres.map((genre, index) => (
-                //   map()은 index를 넘겨준다
-                <li key={index} className="genres__genre">
-                  {genre}{" "}
-                </li>
-              ))}
-            </ul>
-            <p className="movie__summary">{summary.slice(0, 180)}...</p>
-          </div>
+        <img src={poster} alt={title} title={title} />
+        <div className="movie__data">
+          <h3 className="movie__title">{title}</h3>
+          <h5 className="movie__year">{year}</h5>
+          <ul className="genres">
+            {genres.map((genre, index) => (
+              //   map()은 index를 넘겨준다
+              <li key={index} className="genres__genre">
+                {genre}{" "}
+              </li>
+            ))}
+          </ul>
+          <p className="movie__summary">{summary.slice(0, 180)}...</p>
         </div>
       </Link>
     </div>
