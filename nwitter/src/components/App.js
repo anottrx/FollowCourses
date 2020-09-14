de3a46705fbdf04bce02fc50a38b0ad05e74b216
anottrx <anottrx@gmail.com>
2020-09-14 23:04:36 +0900

    #5 Edit Profile
    프로필 이름 변경 후 수정 버튼을 누르면 즉시 반영
    파이어베이스 Cloud Firestore 컬렉션 설정

--- nwitter/src/components/App.js
+++ nwitter/src/components/App.js
@@ -5,21 +5,38 @@ import { authService } from "fbase";
 function App() {
   const [init, setInit] = useState(false);
   const [userObj, setUserObj] = useState(null);
+  // 가장 높은 레벨. 이후 Home, Profile, Navigation 순서대로
 
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       // 상태 변화를 즉시 반영
       if (user) {
-        setUserObj(user);
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        });
       }
       setInit(true);
     });
   }, []);
+  const refreshUser = () => {
+    const user = authService.currentUser;
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    });
+  };
 
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         "Initializing..."
       )}
